9c3450278d006204d4d22ab3b7987a3c5bf6c42f
Vivek Jindal <vjindal@inecta.com>
2022-07-16 17:26:24 +0530

    actions update

---
+++ UploadBlobs/README.md
@@ -0,0 +1,2 @@
+# Upload Blobs
+Prepends release code and uploads the compiled apps to Azure Blob Storage.
---
+++ UploadBlobs/UploadBlobs.ps1
@@ -0,0 +1,59 @@
+# initial configuration
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version 2.0
+$telemetryScope = $null
+$bcContainerHelperPath = $null
+
+# get github secrets
+$script:gitHubSecrets = $env:secrets | ConvertFrom-Json
+
+# secrets config
+$DevOpsRelease = $gitHubSecrets.AZDEVOPSRELEASE
+$DevOpsAccount = $gitHubSecrets.AZDEVOPSACCOUNT
+$DevOpsContainer = $gitHubSecrets.AZDEVOPSCONTAINER
+
+try {
+
+    # import helper function and download bccontainerhelper
+    . (Join-Path -Path $PSScriptRoot -ChildPath "..\AL-Go-Helper.ps1" -Resolve)
+    $baseFolder = $ENV:GITHUB_WORKSPACE
+    $BcContainerHelperPath = DownloadAndImportBcContainerHelper -baseFolder $baseFolder
+
+    # get the algo settingsjson
+    Write-Host -Object "`nParsing AL-Go-Settings.json..."
+    $algosettingsjson = Get-Content -Path "$baseFolder\.github\AL-Go-Settings.json" | ConvertFrom-Json
+
+    # get the list of apps
+    $apps = $algosettingsjson.appFolders -replace "/app", ""
+    Write-Host -Object "List of apps: $($apps -join ",")"
+
+    # copy the blobs to new directory
+    New-Item -Path $baseFolder -Name "release-blobs" -ItemType Directory -Force | Out-Null
+    Copy-Item -Path "$baseFolder\output\Apps\*.app" -Destination "$baseFolder\release-blobs" -Force
+
+    # rename artifacts
+    Write-Host -Object "`nRenaming artifacts for upload to blob storage..."
+    $apps | ForEach-Object {
+        $appname = $_
+        $blobname = Get-ChildItem -Path "$baseFolder\release-blobs\" | Where-Object {$_.Name -like "*$appname*"}
+        $releaseversion = ($blobname.BaseName).Split('_') | Select-Object -Last 1
+        Rename-Item -Path $blobname.FullName -NewName "$baseFolder\release-blobs\$appname.$releaseversion.app" -Verbose
+    }
+
+    # upload artifacts to blob storage
+    Get-ChildItem -Path "$baseFolder\release-blobs\" | ForEach-Object {
+        $uri = $uri = "https://$DevOpsAccount.blob.core.windows.net/$DevOpsContainer/" + $_.Name + $DevOpsRelease
+        Write-Host -Object "Uploading $($_.Name) Azure Blob Storage..."
+        Invoke-WebRequest -Uri $uri -Method Put -InFile $_.FullName -ContentType 'application/json' -Headers @{'Content-Type' = 'application/json'; 'x-ms-blob-type' = 'BlockBlob'}
+    }
+
+}
+catch {
+    OutputError -message $_.Exception.Message
+    Write-Host -Object "Cleaning up inecta apps repository directories..."
+    Set-Location -Path "$baseFolder"
+    Remove-Item -Path "$baseFolder\inecta-apps" -Recurse -Force -ErrorAction SilentlyContinue
+}
+finally {
+    CleanupAfterBcContainerHelper -bcContainerHelperPath $bcContainerHelperPath
+}
