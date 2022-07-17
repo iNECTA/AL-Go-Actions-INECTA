e44d9790b53de1fe130ba743491acff22c38cbc5
Vivek Jindal <vjindal@inecta.com>
2022-07-17 18:16:00 +0530

    update script

--- UpdateCustomerRepository/UpdateCustomerRepository.ps1
+++ UpdateCustomerRepository/UpdateCustomerRepository.ps1
@@ -29,6 +29,8 @@ try {
     Write-Host -Object "Adding version file to customer repository...`n"
     Copy-Item -Path "$baseFolder\inecta-apps\release.version" -Destination $baseFolder -Force
     Remove-Item -Path "$baseFolder\inecta-apps" -Recurse -Force -ErrorAction SilentlyContinue
+    $releaseversion = Get-Content -Path "$baseFolder\release.version"
+    Write-Host -Object "Release version: $releaseversion"
 
     # merge the changes to customer repository
     Set-Location -Path $baseFolder
