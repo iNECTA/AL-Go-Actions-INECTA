43ce2c1a17ea83ea9492dd8f351c2b6af528de26
Benjamin McLemore <ben@inecta.com>
2025-05-12 14:33:34 -0400

    test without staging

--- CloneSourceCode/CloneSourceCode.ps1
+++ CloneSourceCode/CloneSourceCode.ps1
@@ -101,7 +101,7 @@ try {
     "success" | Out-File -FilePath "$ENV:ProgramData\BcContainerHelper\INECTA\SIMPMgmtUpdate.log" -NoNewline -Force
     $ErrorActionPreference = "Continue"
     Set-Location -Path "$home\desktop\SCRIPTS"; . ./Simpmgmt.ps1
-    . "$home\Desktop\SCRIPTS\start.ps1"
+    # TODO: Ben
     $ErrorActionPreference = "Stop"
 
     # run get-app
--- CloneSourceCode/CloneSourceCode2.ps1
+++ CloneSourceCode/CloneSourceCode2.ps1
@@ -64,7 +64,7 @@ try {
     "success" | Out-File -FilePath "$ENV:ProgramData\BcContainerHelper\INECTA\SIMPMgmtUpdate.log" -NoNewline -Force
     $ErrorActionPreference = "Continue"
     Set-Location -Path "$home\desktop\SCRIPTS"; . ./Simpmgmt.ps1
-    . "$home\Desktop\SCRIPTS\start.ps1"
+    # TODO: Ben
     $ErrorActionPreference = "Stop"
 
   
